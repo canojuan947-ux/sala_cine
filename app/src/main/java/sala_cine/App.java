1b9f424616056b9c774d3a8ef7bb6f4277a791a5
sandr <canojuan947@gmail.com>
2026-05-09 13:28:56 -0500

    Agregar el archivo con la sala

--- app/src/main/java/sala_cine/App.java
+++ app/src/main/java/sala_cine/App.java
@@ -1,8 +1,26 @@
 package sala_cine;
-
+import java.io.*;
 public class App {
 
+    // Variables globales
+    public static File ARCHIVOLECTURA = new File("puestosDisponibles.txt");
+
     public static void main(String[] args) {
-        
+
+        // Leer el archivo con las sillas disponibles
+        try (FileReader fr = new FileReader(ARCHIVOLECTURA);
+             BufferedReader br = new BufferedReader(fr)    
+    ) {
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error de lectura de archivos " + e);
+        } catch (Exception e){
+            System.out.println("Error " + e);
+        }
     }
 }
