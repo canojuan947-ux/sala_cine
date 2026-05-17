ef2bde07b371ea3cd31c8c83669512f59d921d74
sandr <canojuan947@gmail.com>
2026-05-16 22:32:09 -0500

    Corrección para mostar la sala

--- app/src/main/java/sala_cine/App.java
+++ app/src/main/java/sala_cine/App.java
@@ -6,21 +6,19 @@ import javax.swing.*;
 public class App {
 
     // Variables globales
-    public static File ARCHIVOLECTURA = new File("puestosDisponibles.txt");
+    public static String [][] SALA = new String[6][9];
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-// Leer el archivo con las sillas disponibles
-try (FileReader fr = new FileReader(ARCHIVOLECTURA);
-    BufferedReader br = new BufferedReader(fr)
-) {                                                                                             
+    // Leer el archivo con las sillas disponibles
+    try {                                                                            
         // Variables
         int cantidaDeSillas = 0, fila = 0, columna = 0, continuar = 0;
         String nombre = "";
 
         Scanner sc = new Scanner(System.in);
 
-        // mostrar menu
+        // menú bienvenida
         System.out.println("\n======================\n" + 
                            "¡Bienvenido a Cinemax!\n" +
                            "======================\n");
@@ -37,22 +35,22 @@ try (FileReader fr = new FileReader(ARCHIVOLECTURA);
                 
                 if (opcionMenu == 1) {
 
-                    System.out.print("\nIngrese su nombre completo, por favor.\n-> ");
-                    nombre = sc.nextLine();
-                    do{
+                System.out.print("\nIngrese su nombre completo, por favor.\n-> ");
+                nombre = sc.nextLine();
 
-                        String linea;
-
-                        while ((linea = br.readLine()) != null) {
-                            System.out.println(linea);
-                        }
-
-                    do {fila = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de la fila de su asiento"));
+                String muestraDeSala = mostarSala(SALA);
+                System.out.println(muestraDeSala);
+                do{
+                    do {System.out.println("Ingrese el numero de la fila de su asiento");
+                        fila = sc.nextInt(); sc.nextLine();
                     } while (fila < 1 || fila > 6);
-                    do {columna = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de la columna de su asiento"));
+                    do {System.out.println("Ingrese el numero de la columna de su asiento");
+                        columna = sc.nextInt();sc.nextLine();
                     } while (columna < 1 || columna > 9);
-                    continuar = JOptionPane.showConfirmDialog(null, "¿Desea reservar otra silla?");
-                    }while(continuar == 0);
+                    do{System.out.println("¿Desea reservar otra silla?\n1. Si\n2. No");   
+                        continuar = sc.nextInt(); sc.nextLine();              
+                    } while (continuar < 1 || continuar > 2);
+                }while(continuar == 1);
 
                 } else {
                     String salida = mostrarMenu(false);
@@ -64,14 +62,12 @@ try (FileReader fr = new FileReader(ARCHIVOLECTURA);
                 System.out.println(salida);;
             }
 
-} catch (IOException e) {
-    System.out.println("Error de lectura de archivos " + e);
-} catch (Exception e){
-    System.out.println("Error: " + e.getMessage());
-}
-        
-        
+    System.out.println();reservarSilla(fila, columna);
+    
+    } catch (Exception e){
+        System.out.println("Error: " + e.getMessage());
     }
+}
 
     // Funcion para mostrar el menú
     public static String mostrarMenu (boolean a){
@@ -94,4 +90,40 @@ try (FileReader fr = new FileReader(ARCHIVOLECTURA);
             return salidaMenu;
         }
     } 
+
+    // Funcion para guarfar la silla
+    public static void reservarSilla (int x, int y){
+
+        SALA[x - 1][y - 1] = "X";
+    }
+
+    // Función para mostar la sala
+    public static String mostarSala(String[][] sala) {
+
+        String salaMod ="                  PANTALLA\n"+
+                        "=================================================\n"+
+                        "     1   2   3     4   5   6     7   8   9\n";
+
+        for (int i = 0; i < sala.length; i++) {
+
+            salaMod += (i+1) + "   ";
+
+            for (int j = 0; j < sala[0].length; j++) {
+
+                SALA[i][j] = " ";
+
+                salaMod += "[" + SALA[i][j] + "] ";
+                    if (((j+1) % 3)== 0){
+                        salaMod += "  ";
+                    }
+                    
+            }
+            salaMod += "\n";
+        }
+
+        salaMod +="=================================================\n\n" + 
+                  "[ ] = Asiento disponible\n[X] = Asiento ocupado\n";  
+        return salaMod;
+    }
+
 }
